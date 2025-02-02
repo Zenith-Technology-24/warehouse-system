8f5b2620166b5329e5810db988c5ea227c25cead
novidash <spacecat34@gmail.com>
2025-02-02 18:51:50 +0800

    fix some stuff

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, protocol, session, ipcMain } from "electron";
+import { app, BrowserWindow } from "electron";
 import * as path from "path";
 
 let mainWindow: Electron.BrowserWindow | null;
