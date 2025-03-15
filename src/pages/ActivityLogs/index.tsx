fc66cf70fe2a55d5659cd076fd1ba3ed154b04cd
Bobert <salvo.robertroy@gmail.com>
2025-03-15 17:57:29 +0800

    #11: added table and activityLogsApi file

--- src/pages/ActivityLogs/index.tsx
+++ src/pages/ActivityLogs/index.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import Header from "../../components/Header"
 import Search from "../../components/Search"
 import PrimaryButton from "../../components/buttons/PrimaryButton"
@@ -7,6 +7,8 @@ import exportToExcel from "../../components/ExportToExcel"
 import moment from "moment"
 import { exportExpenses } from "../../api/expenses/expensesApi"
 import Table from "../../components/Table"
+import { useQuery } from "@tanstack/react-query"
+import { fetchActivityLogs } from "../../api/activityLogs/activityLogsApi"
 
 const ActivityLogs = () => {
     
@@ -15,14 +17,19 @@ const ActivityLogs = () => {
     const [search, setSearch] = useState<string>('')
     const [status, setStatus] = useState<string>('active')
     const [page, setPage] = useState<number>(1)
-    // const { data: rows, refetch } = useQuery({
-    //     queryKey: ["receipt", search, page, limit, status],
-    //     queryFn: () => fetchReceipt({ search, page, limit, status }) as any,
-    // });
+    const [limit, setLimit] = useState<number>(10)
+    const { data: rows, refetch } = useQuery({
+      queryKey: ["activityLogs", search, page, limit, status],
+      queryFn: () => fetchActivityLogs({ search, page, limit, status }) as any
+    });
+    
     const handleSearch = (searchInput = '') => {
         setSearch(searchInput)
     };
 
+    const handleChangePage = (page: number) => {
+        setPage(page)
+    }
     const handleExport = ({ toExport, start_date, end_date }: any) => {
         const headers = [
             { header: 'Expenses ID', key: 'id', width: 10 },
@@ -73,6 +80,47 @@ const ActivityLogs = () => {
         exportToExcel({ data, headers, filename: `${status}-expenses-${start_date}-to-${end_date}` })
     }
 
+    const columns = useMemo(() => {
+        return [
+          {
+            label: 'Date',
+            name: 'date',
+            render(row: object, value: string, rowIndex: number) {
+              return (
+                <div className="font-normal">
+                  {value}
+                </div>
+              )
+            }
+          },
+          {
+            label: 'Activity',
+            name: 'activity',
+            render(row: object, value: string, rowIndex: number) {
+              return (
+                <div className="font-normal">
+                  {value}
+                </div>
+              )
+            }
+          },
+          {
+            label: 'Performed By',
+            name: 'performedBy',
+            render(row: object, value: { userName: string, role: string }, rowIndex: number) {
+              return (
+                <div className="font-normal flex flex-col">
+                  <p>{value?.userName}</p>
+                  <p className="text-xs text-gray-700">{value?.role}</p>
+                </div>
+              )
+            }
+          },
+        ]
+      }, [rows])
+
+
+
     return (
         <>
             <ExportModal
@@ -84,38 +132,40 @@ const ActivityLogs = () => {
                 exportFunction={exportExpenses}
             />
             
-            <div className="flex flex-row justify-between">
-                <Header title={'Activity Logs'} description={'Showing activity logs'} />
-                <div className="flex gap-5 items-center">
-                    <input
-                        name="end_date"
-                        value={date === null ? '' : date}
-                        onChange={(e) => setDate(e.target.value)}
-                        type="date"
-                        id="end_date"
-                        className="bg-transparent text-gray-500 h-12 border border-gray-300 p-4 mb-1 rounded-md"
-                        placeholder="Enter End Date"
-                        required
-                    />
-                    <Search
-                        handleFetchData={handleSearch}
-                    />
-                    <PrimaryButton
-                        onClick={() => { setIsExportModalOpen(true)}}
-                        text={'Export'} 
-                    />
+            <div className="flex flex-row justify-between items-center">
+                <div className="w-48">
+                  <Header title={'Activity Logs'} description={'Showing all activity logs'} />  
+                </div>
+                <div className="flex gap-5">
+                  <input
+                    name="end_date"
+                    value={date === null ? '' : date}
+                    onChange={(e) => setDate(e.target.value)}
+                    type="date"
+                    id="end_date"
+                    className="bg-transparent text-gray-500 h-12 border border-gray-300 p-4 mb-1 rounded-md"
+                    placeholder="Enter End Date"
+                    required
+                  />
+                  <Search
+                    handleFetchData={handleSearch}
+                  />
+                  <PrimaryButton
+                      onClick={() => { setIsExportModalOpen(true)}}
+                      text={'Export'} 
+                  />
                 </div>
             </div>
-            {/* <Table
+            <Table
                 currentPage={page}
                 setCurrentPage={setPage}
-                // totalRows={rows?.data?.length || 1}
+                totalRows={rows?.data?.length || 1}
                 columns={columns}
                 rows={rows}
                 rowsPerPage={limit}
                 totalPages={rows?.totalPages}
                 onPageChange={handleChangePage}
-            /> */}
+            />
         </>
     )
 }
