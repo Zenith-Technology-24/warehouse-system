193df13059587d4f108e7ffdf7aa60c01e30e8f7
Gil lopez <52746589+Phnumbahwan@users.noreply.github.com>
2025-04-01 01:25:36 +0800

    Sprint 2 (#110)
    * Revert code
    
    * Fixed and complate issuance feature
    
    * Fixed size duplicate component
    
    ---------
    
    Co-authored-by: Gil Pacturan <gilpacturan@gmail.com>

--- src/pages/Issuance/Create/index.tsx
+++ src/pages/Issuance/Create/index.tsx
@@ -266,7 +266,7 @@ const CreateIssuance: React.FC = () => {
                                                                 <div className="flex h-auto flex-col py-3 col-span-2">
                                                                     <label className="pb-2" htmlFor={`endUsers[${index}].inventory[${_index}].receiptRef`}>Receipt Ref</label>
                                                                     <DropdownWithSearch
-                                                                        values={values}
+                                                                        formikSelectedValue={values?.endUsers[index].inventory[_index].receiptRef}
                                                                         _index={_index}
                                                                         placeholder="Receipt Ref"
                                                                         name={`endUsers[${index}].inventory[${_index}].receiptRef`}
@@ -299,7 +299,7 @@ const CreateIssuance: React.FC = () => {
                                                                 <div className="flex h-auto flex-col py-3">
                                                                     <label className="pb-2" htmlFor={`endUsers[${index}].inventory[${_index}].name`}>Item Name</label>
                                                                     <DropdownWithSearch
-                                                                        values={values}
+                                                                        formikSelectedValue={values?.endUsers[index]?.inventory[_index].name}
                                                                         _index={index}
                                                                         placeholder="Item Name"
                                                                         name={`endUsers[${index}].inventory[${_index}].name`}
@@ -397,47 +397,47 @@ const CreateIssuance: React.FC = () => {
                                                                         <ErrorMessage className="text-red-400" name={`endUsers[${index}].inventory[${_index}].amount`} component="div" />
                                                                     </div>
                                                                 </div>
+                                                                <div className="flex flex-row gap-5">
+                                                                    <div className="py-6">
+                                                                        <div
+                                                                            onClick={() => {
+                                                                                if (values.endUsers[index].inventory.length > 1) {
+                                                                                    const updatedInventory = [...values.endUsers[index].inventory];
+                                                                                    updatedInventory.splice(_index, 1);
+                                                                                    setFieldValue(`endUsers[${index}].inventory`, updatedInventory);
+                                                                                }
+                                                                            }}
+                                                                            className={`flex flex-row gap-2 items-center text-sm text-red-300 ${values.inventories?.length > 1 && 'hover:text-red-400'} cursor-pointer`}>
+                                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5">
+                                                                                <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                                            </svg>
+                                                                            <p>Remove Item</p>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div onClick={() => setFieldValue(`endUsers[${index}].inventory`, [...values.endUsers[index].inventory, {
+                                                                        id: '',
+                                                                        receiptRef: '',
+                                                                        name: '',
+                                                                        sizeType: '',
+                                                                        item: {
+                                                                            location: '',
+                                                                            size: '',
+                                                                            unit: '',
+                                                                            quantity: 1,
+                                                                            price: 0,
+                                                                            amount: 0
+                                                                        }
+                                                                    }])} className="flex flex-row gap-2 items-center text-sm text-gray-500 hover:text-gray-800 cursor-pointer">
+                                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                                                        </svg>
+                                                                        <p>Add Item</p>
+                                                                    </div>
+                                                                </div>
                                                             </div>
                                                         )
                                                     })
                                                 }
-                                                <div className="flex flex-row gap-5">
-                                                    <div className="py-6">
-                                                        <div
-                                                            onClick={() => {
-                                                                if (values.endUsers[index].inventory.length > 1) {
-                                                                    const updatedinventory = [...values.endUsers[index].inventory];
-                                                                    updatedinventory.splice(index, 1);
-                                                                    setFieldValue(`endUsers[${index}].inventory`, updatedinventory);
-                                                                }
-                                                            }}
-                                                            className={`flex flex-row gap-2 items-center text-sm text-red-300 ${values.inventories?.length > 1 && 'hover:text-red-400'} cursor-pointer`}>
-                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5">
-                                                                <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                                            </svg>
-                                                            <p>Remove Item</p>
-                                                        </div>
-                                                    </div>
-                                                    <div onClick={() => setFieldValue(`endUsers[${index}].inventory`, [...values.endUsers[index].inventory, {
-                                                        id: '',
-                                                        receiptRef: '',
-                                                        name: '',
-                                                        sizeType: '',
-                                                        item: {
-                                                            location: '',
-                                                            size: '',
-                                                            unit: '',
-                                                            quantity: 1,
-                                                            price: 0,
-                                                            amount: 0
-                                                        }
-                                                    }])} className="flex flex-row gap-2 items-center text-sm text-gray-500 hover:text-gray-800 cursor-pointer">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                                                        </svg>
-                                                        <p>Add Item</p>
-                                                    </div>
-                                                </div>
                                             </div >
                                         )
                                     })}
