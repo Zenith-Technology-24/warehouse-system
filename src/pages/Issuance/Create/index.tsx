89fbfb24b801940991486f97ab1a48f68a2d0114
Bobert <salvo.robertroy@gmail.com>
2025-04-01 23:00:49 +0800

    removed inventory

--- src/pages/Issuance/Create/index.tsx
+++ src/pages/Issuance/Create/index.tsx
@@ -119,10 +119,6 @@ const CreateIssuance: React.FC = () => {
                             ...values,
                             issuanceDate: values.issuanceDate ? `${values.issuanceDate}T00:00:00.000Z` : null,
                             validityDate: values.validityDate ? `${values.validityDate}T00:00:00.000Z` : null,
-                            inventory: values.endUsers[0].inventory.map((inv: any) => ({
-                                ...inv,
-                                amount: inv.price * inv.quantity,
-                            }))
                         };
                         createIssuanceMutation.mutate(formattedValues)
                     }}
