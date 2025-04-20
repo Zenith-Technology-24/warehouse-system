1a5bfb9c729ce09caf85cd598315988cfb6415c6
Gil Pacturan <gilpacturan@gmail.com>
2025-04-20 22:38:46 +0800

    Update icons for inventory view

--- src/components/DashboardCard.tsx
+++ src/components/DashboardCard.tsx
@@ -54,7 +54,7 @@ const isActive = (active: boolean, type: string) => {
 
 const DashboardCard: React.FC<Props> = ({ active = null, icon, title, value, type, handleClick }) => {
     return (
-        <div onClick={handleClick} className={`shadow-md hover:shadow-lg flex flex-col justify-center gap-2 rounded-lg items-start p-4 ${getStyle(type)} ${isActive(active, type)}`}>
+        <div onClick={handleClick} className={`shadow-md hover:shadow-lg flex flex-col justify-start gap-2 rounded-lg items-start p-4 ${getStyle(type)} ${isActive(active, type)}`}>
             {icon}
             <p className="text-gray-400 font-medium">{title}</p>
             <h1 className="font-medium text-gray-600 text-right text-3xl">{value}</h1>
